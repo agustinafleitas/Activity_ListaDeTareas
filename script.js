64999e5a71f8fc122f29b6c6d786b3c82e82f431
Agustina Fleitas <141256804+agustinafleitas@users.noreply.github.com>
2023-10-04 10:18:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 let NotasInput=document.getElementById("notas");
 let BotonEnviar=document.getElementById("btn");
 let ContainerLista=document.getElementById("textoContainer");
-let ApiURL='https://picsum.photos/100';
 
-BotonEnviar.addEventListener("click", (e)=>{
+BotonEnviar.addEventListener("click", async (e)=>{
     e.preventDefault();
 
     let MostrarNotas=NotasInput.value
+
+    const randomNum = Math.floor(Math.random() * 1000); //Generar un número al azar
+    let ApiURL=`https://picsum.photos/250?random=${randomNum}`; //El URL de la API se ajustó para que muestre imágenes de 300px y que muestre una imagen al azar segun la variable creada
     
+    try {
+        let response = await fetch(ApiURL);
+        if (!response.ok) {
+            throw new Error("Hubo un error al obtener la imagen.");
+        }
+        
+        const imageBlob = await response.blob(); 
+        const imageUrl = URL.createObjectURL(imageBlob); 
+
     let CrearEspacioNota=document.createElement("div") //Crear div que contenga las notas, imagen y botones
 
     let Imagen=document.createElement("img")
     Imagen.src=ApiURL //Al elemento "img" se le asigna una imagen de la API
 
     let NotaParrafo=document.createElement("p") 
-    NotaParrafo.textContent = `Nota: ${MostrarNotas}`;//El texto redactado en el campo de texto
+    NotaParrafo.textContent = `Nota: ${MostrarNotas}`;//Mostrar el texto redactado en el campo de texto
 
     //Botones de eliminar y completar 
     let BotonCompletar=document.createElement("button");
     BotonCompletar.textContent = "Completar";
+    BotonCompletar.addEventListener("click", (e)=>{
+        NotaParrafo.classList.toggle("completado");
+    });
+
     let BotonEliminar=document.createElement("button");
     BotonEliminar.textContent = "Eliminar";
+    BotonEliminar.addEventListener("click", (e)=>{
+        ContainerLista.removeChild(CrearEspacioNota)
+    });
 
     CrearEspacioNota.appendChild(NotaParrafo)
     CrearEspacioNota.appendChild(Imagen)
@@ -30,4 +48,7 @@ BotonEnviar.addEventListener("click", (e)=>{
     ContainerLista.appendChild(CrearEspacioNota)
 
     NotasInput.value='' //Se limpia el campo de texto luego de enviar lo escrito
+    }catch(error){
+        console.error(error)
+    }
 });
